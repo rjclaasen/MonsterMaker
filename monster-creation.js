1ee3e56bce8a5af2605b21b16502f40f1d98f844
Raphaël Claasen <raphael.claasen@gmail.com>
2017-12-04 16:14:46 +0100

    Slider removed.
    Seemed too clunky. Users will know rough CR of target anyway & should be
    able to input directly. Slider does not add value.

--- monster-creation.js
+++ monster-creation.js
@@ -1,30 +1,3 @@
-function setupSlider() {
-  var realValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33];
-  var labelValues = [0, 1/8, 1/4, 1/2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
-  var slider = $("#crSlider");
-  var textBox = $("#crTextBox");
-
-  slider.slider({
-    max: 33,
-    min: 0,
-    step: 1,
-    formatter: function(val) {
-      return labelValues[val];
-    }
-  }).on('change', function(data){
-    textBox.val(labelValues[data.value.newValue]);
-  });
-
-  function adjustSlider() {
-    var newValue = labelValues.indexOf(textBox[0].valueAsNumber);
-    slider.slider('setValue', newValue, true);
-    console.log(textBox[0].valueAsNumber + " " + labelValues.indexOf(textBox[0].valueAsNumber) + " " + slider[0].value);
-  }
-
-  textBox.keyup(adjustSlider);
-  textBox.click(adjustSlider);
-}
-
 $(document).ready(function() {
-  setupSlider();
+
 });
