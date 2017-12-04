73b1cf8d79d94321cf9c946f2a0ccf91e96f2936
Raphaël Claasen <raphael.claasen@gmail.com>
2017-12-04 20:45:15 +0100

    Offensive and defensive CR now change with average CR.

--- monster-creation.js
+++ monster-creation.js
@@ -1,3 +1,29 @@
-$(document).ready(function() {
+function setupChallengeRatings() {
+  var crTextBox = $("#crTextBox");
+  var ocrTextBox = $("#ocrTextBox");
+  var dcrTextBox = $("#dcrTextBox");
 
+  var oldBaseCR = parseInt(crTextBox.val());
+  var newBaseCR = oldBaseCR;
+
+  crTextBox.on('input', function() {
+    oldBaseCR = newBaseCR;
+    newBaseCR = parseInt($(this).val());
+    var dCR = newBaseCR - oldBaseCR;
+
+    if(!(isNaN(parseInt(ocrTextBox.val())))) {
+      ocrTextBox.val(parseInt(ocrTextBox.val()) + dCR);
+    } else {
+      ocrTextBox.val(newBaseCR);
+    }
+    if(!isNaN(parseInt(dcrTextBox.val()))) {
+      dcrTextBox.val(parseInt(dcrTextBox.val()) + dCR);
+    } else {
+      dcrTextBox.val(newBaseCR);
+    }
+  });
+}
+
+$(document).ready(function() {
+  setupChallengeRatings();
 });
