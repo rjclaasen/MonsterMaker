b19e22383f8b94c5603356703c09be5ff8e321ae
Raphaël Claasen <raphael.claasen@gmail.com>
2017-12-04 22:16:15 +0100

    Size effects now explained & update when change.

--- monster-creation.js
+++ monster-creation.js
@@ -1,15 +1,53 @@
-var ocr;
-var dcr;
-var cr;
+function setupSizeChoiceEffects() {
+  var sizeSelect = $("#sizeSelect");
+  var numDiceFromSizeText = $("#numDiceFromSizeText");
+  var hitDieSize = $("#hitDieSize");
+
+  sizeSelect.on('change', function() {
+    var numDice = "1"; // This is a string because gargantuan can have 4 or more.
+    var hitDie = "d8";
+    var valueSelected = this.value;
+
+    switch(valueSelected) {
+      case "tiny":
+        numDice = "1";
+        hitDie = "d4";
+        break;
+      case "small":
+        numDice = "1";
+        hitDie = "d6";
+        break;
+      case "medium":
+        numDice = "1";
+        hitDie = "d8";
+        break;
+      case "large":
+        numDice = "2";
+        hitDie = "d10";
+        break;
+      case "huge":
+        numDice = "3";
+        hitDie = "d12";
+        break;
+      case "gargantuan":
+        numDice = "4";
+        hitDie = "d20";
+        break;
+    }
+
+    numDiceFromSizeText.text(numDice);
+    hitDieSize.text(hitDie);
+  });
+}
 
 function setupChallengeRatings() {
   var crTextBox = $("#crTextBox");
   var ocrTextBox = $("#ocrTextBox");
   var dcrTextBox = $("#dcrTextBox");
 
-  ocr = parseInt(ocrTextBox.val());
-  dcr = parseInt(dcrTextBox.val());
-  cr = parseInt(crTextBox.val());
+  var ocr = parseInt(ocrTextBox.val());
+  var dcr = parseInt(dcrTextBox.val());
+  var cr = parseInt(crTextBox.val());
 
   var docr = ocr - cr;
   var ddcr = dcr - cr;
@@ -48,5 +86,6 @@ function setupChallengeRatings() {
 }
 
 $(document).ready(function() {
+  setupSizeChoiceEffects();
   setupChallengeRatings();
 });
