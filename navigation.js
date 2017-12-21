450656b1f275a1a753643650a6b7b260a0d19728
Raphaël Claasen <raphael.claasen@gmail.com>
2017-12-21 22:18:54 +0100

    Buttons now adapt visually to nav-tabs positioning.

--- navigation.js
+++ navigation.js
@@ -1,8 +1,36 @@
+function ShowOrHideNextSubmit() {
+  var current = $(".nav-tabs > .nav-item > .active").parent()
+
+  if(current.is($("#navTabs > .nav-item").last())) {
+    $("#btnPrevious").prop("disabled", false);
+    $("#btnNext").hide();
+    $("#btnSubmit").show();
+  }
+  else if(current.is($("#navTabs > .nav-item").first())) {
+    console.log("beep");
+    $("#btnPrevious").prop("disabled", true);
+    $("#btnNext").show();
+    $("#btnSubmit").hide();
+  }
+  else {
+    $("#btnPrevious").prop("disabled", false);
+    $("#btnNext").show();
+    $("#btnSubmit").hide();
+  }
+}
+
 $(document).ready(function(){
+  // Event setup
   $("#btnPrevious").click(function(){
-    $(".nav-tabs > .nav-item > .active").parent().prev("li").find("a").trigger("click");
+    $("#navTabs > .nav-item > .active").parent().prev("li").find("a").trigger("click");
   });
   $("#btnNext").click(function(){
-    $(".nav-tabs > .nav-item > .active").parent().next("li").find("a").trigger("click");
+    $("#navTabs > .nav-item > .active").parent().next("li").find("a").trigger("click");
   });
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    ShowOrHideNextSubmit();
+  });
+
+  // Page setup
+  ShowOrHideNextSubmit();
 });
