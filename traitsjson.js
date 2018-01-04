e84779bccca851f6f199ed47070c77aede621e4f
Raphaël Claasen <raphael.claasen@gmail.com>
2018-01-04 17:29:11 +0100

    Trait effects added.

--- traitsjson.js
+++ traitsjson.js
@@ -20,18 +20,22 @@ function SetupSelectOptionEffects(){
   var traitsSelect = $("#traitsSelect");
   var exampleMonsterText = $("#exampleMonsterText");
   var crEffectText = $("#crEffectText");
+  var traitEffectText = $("#traitEffectText");
 
   traitsSelect.on("change", function() {
     var exampleMonster = "Vortigaunt"
     var crEffect = "None."
+    var traitEffect = "None."
     var valueSelected = this.value;
 
     var matchingJsonObject = $.grep(jsonData, function(obj){return obj.id == valueSelected;})[0];
     exampleMonster = matchingJsonObject.exampleMonster;
     crEffect = matchingJsonObject.crEffect;
+    traitEffect = matchingJsonObject.traitEffect;
 
     exampleMonsterText.val(exampleMonster);
     crEffectText.text(crEffect);
+    traitEffectText.text(traitEffect); //TODO: Ensure markup can be used to highlight spell names, recharge times, etc.
   });
 }
 
