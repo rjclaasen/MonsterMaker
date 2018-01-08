fcba02175381ab1cf38358dd40371928a3c82b3d
Raphaël Claasen <raphael.claasen@gmail.com>
2018-01-08 18:48:01 +0100

    Values @ Details tab are updated when navigated to.

--- public/monster-creation_details.js
+++ public/monster-creation_details.js
@@ -1,7 +1,18 @@
 function SetupLoading() {
   $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
     if($(e.target).attr('href') == "#details") {
-      // Do the thing
+      // Read the values from the Basics page and put them into the textboxes on the Details page.
+      $('#nameTextBox_details').val($('#nameTextBox_basics').val());
+      $('#ocrTextBox_details').val($('#ocrTextBox_basics').val());
+      $('#dcrTextBox_details').val($('#dcrTextBox_basics').val());
+      $('#crTextBox_details').val($('#crTextBox_basics').val());
+
+      $('#proficiencyText_details').val($('#proficiencyText_basics').val());
+      $('#armorclassText_details').val($('#armorclassText_basics').val());
+      $('#hitpointsText_details').val($('#hitpointsText_basics').val());
+      $('#atkText_details').val($('#atkText_basics').val());
+      $('#dmgText_details').val($('#dmgText_basics').val());
+      $('#dcText_details').val($('#dcText_basics').val());
     }
   })
 }
