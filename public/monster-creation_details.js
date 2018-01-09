07bea53e9b35d6d09a7b16066f09a44549fb747d
Raphaël Claasen <raphael.claasen@gmail.com>
2018-01-10 00:18:27 +0100

    Details tab: HP gets updated on AC change.

--- public/monster-creation_details.js
+++ public/monster-creation_details.js
@@ -1,3 +1,10 @@
+var offensiveCR;
+var defensiveCR;
+var averageCR;
+var suggestedAC;
+var minHP;
+var maxHP;
+
 function SetupLoading() {
   $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
     if($(e.target).attr('href') == "#details") {
@@ -13,10 +20,43 @@ function SetupLoading() {
       $('#atkText_details').val($('#atkText_basics').val());
       $('#dmgText_details').val($('#dmgText_basics').val());
       $('#dcText_details').val($('#dcText_basics').val());
+
+      // Set variables
+      offensiveCR = parseInt($('#ocrTextBox_details').val());
+      defensiveCR = parseInt($('#dcrTextBox_details').val());
+      averageCR = parseInt($('#crTextBox_details').val());
+      suggestedAC = parseInt($('#armorclassText_details').val());
+      var hpRange = $('#hitpointsText_details').val().split('-');
+      minHP = parseInt(hpRange[0]);
+      maxHP = parseInt(hpRange[1]);
     }
   })
 }
 
+function SetupHP() {
+  var acTextBox = $('#armorclassText_details');
+  var hitpointsTooltip = $('#hitpointsSuggestion');
+  hitpointsTooltip.hide();
+
+  acTextBox.on('input', function() {
+    var userAC = parseInt(acTextBox.val()); // The AC chosen by the user.
+    var dcrAdjustment = Math.floor((userAC - suggestedAC) / 2);
+    if(dcrAdjustment < 0) {
+      hitpointsTooltip.show();
+    } else {
+      hitpointsTooltip.hide();
+    }
+
+    var suggestedHpRange = CRtoHP(defensiveCR + dcrAdjustment);
+    $('#hitpointsText_details').val(RangeToString(suggestedHpRange));
+  });
+
+  $('#hitpointsText_details').on('input', function() {
+    console.log('boop');
+  });
+}
+
 $(document).ready(function() {
-  SetupLoading()
+  SetupLoading();
+  SetupHP();
 });
