c7dde87907492d083cfb0b5d736bbc2b1f3d5f19
Raphaël Claasen <raphael.claasen@gmail.com>
2018-02-26 20:08:26 +0100

    Use SSL and the Puma webserver in production

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,4 @@
+workers ENV.fetch("WEB_CONCURRENCY") { 2 } unless Gem.win_platform?
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -6,6 +7,9 @@
 #
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
+
+preload_app!
+rackup DefaultRackup
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -47,9 +51,11 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # or connections that may have been created at application boot, as Ruby
 # cannot share connections between processes.
 #
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-# end
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
 #
 
 # Allow puma to be restarted by `rails restart` command.
