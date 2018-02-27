2afa64f2e27b66a2492e877ef659246067b38112
Raphaël Claasen <raphael.claasen@gmail.com>
2018-02-27 16:33:30 +0100

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,9 +62,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "gm-tools_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'gm-tools.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :addresses            => "smtp.sendgrid.net",
+    :port                 => "587",
+    :authentication       => :plain,
+    :user_name            => ENV.fetch("SENDGRID_USERNAME"),
+    :password             => ENV.fetch("SENDGRID_PASSWORD"),
+    :domain               => "heroku.com",
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
