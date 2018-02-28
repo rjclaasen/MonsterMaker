1b7f57abdee727f891761da39b17fbe519cb989f
Raphaël Claasen <raphael.claasen@gmail.com>
2018-02-28 18:02:39 +0100

    Added validation for image type and size

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -4,4 +4,14 @@ class Monster < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 255 }
+  validate  :picture_size
+
+  private
+
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
