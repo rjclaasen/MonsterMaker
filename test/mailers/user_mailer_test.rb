fa6866536421372db7ab87f74b6ea15988a8d21a
Raphaël Claasen <raphael.claasen@gmail.com>
2018-03-05 21:45:19 +0100

    Improve user mailer unit tests
    Test queue
    Test both text and html versions.
    Add more comments.

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,3 +1,5 @@
+# Find more about how to test this at http://guides.rubyonrails.org/testing.html#testing-your-mailers
+
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
@@ -7,23 +9,52 @@ class UserMailerTest < ActionMailer::TestCase
   end
 
   test "account_activation" do
+    # Create the email
     @user.activation_token = User.new_token
-    mail = UserMailer.account_activation(@user)
-    assert_equal "Account activation", mail.subject
-    assert_equal [@user.email], mail.to
-    assert_equal ["noreply@monstermaker.org"], mail.from
-    assert_match @user.name,               mail.body.encoded
-    assert_match @user.activation_token,   mail.body.encoded
-    assert_match CGI.escape(@user.email),  mail.body.encoded
+    email = UserMailer.account_activation(@user)
+
+    # Send email and test queue
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    # Test headers of sent email
+    assert_equal ["noreply@monstermaker.org"], email.from
+    assert_equal [@user.email], email.to
+    assert_equal "Account activation", email.subject
+
+    # Test text body of sent email
+    assert_match @user.name,               email.text_part.body.to_s
+    assert_match @user.activation_token,   email.text_part.body.to_s
+    assert_match CGI.escape(@user.email),  email.text_part.body.to_s
+
+    # Test html body of sent email
+    assert_match @user.name,               email.html_part.body.to_s
+    assert_match @user.activation_token,   email.html_part.body.to_s
+    assert_match CGI.escape(@user.email),  email.html_part.body.to_s
   end
 
   test "password_reset" do
+    # Create the email
     @user.reset_token = User.new_token
-    mail = UserMailer.password_reset(@user)
-    assert_equal "Password reset", mail.subject
-    assert_equal [@user.email], mail.to
-    assert_equal ["noreply@monstermaker.org"], mail.from
-    assert_match @user.reset_token,        mail.body.encoded
-    assert_match CGI.escape(@user.email),  mail.body.encoded
+    email = UserMailer.password_reset(@user)
+
+    # Send email and test queue
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    # Test headers of sent email
+    assert_equal ["noreply@monstermaker.org"], email.from
+    assert_equal [@user.email], email.to
+    assert_equal "Password reset", email.subject
+
+    # Test text body of sent email
+    assert_match @user.reset_token,        email.text_part.body.to_s
+    assert_match CGI.escape(@user.email),  email.text_part.body.to_s
+
+    # Test html body of sent email
+    assert_match @user.reset_token,        email.html_part.body.to_s
+    assert_match CGI.escape(@user.email),  email.html_part.body.to_s
   end
 end
