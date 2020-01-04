2626bf8cbdd1191f029af91e841af33a3d42ab44
rjclaasen <raphael.claasen@gmail.com>
2020-01-04 16:00:09 +0100

    Remove user_id requirement of monster

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -2,7 +2,6 @@ class Monster < ApplicationRecord
   belongs_to :user
   default_scope -> { order(name: :asc) }
   mount_uploader :picture, PictureUploader
-  validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 255 }
   validate  :picture_size
 
