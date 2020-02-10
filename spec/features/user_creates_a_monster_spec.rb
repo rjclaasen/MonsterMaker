fff13d9d2d65464e8c64e3d54a55ce889018337d
rjclaasen <raphael.claasen@gmail.com>
2020-02-10 21:49:15 +0100

    Refactor user_creates_a_monster_spec

--- spec/features/user_creates_a_monster_spec.rb
+++ spec/features/user_creates_a_monster_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'User creates a monster' do
-  scenario 'they see the monster on the page' do
+  scenario 'they see the name on the page' do
     visit new_monster_path
 
     fill_in 'Name', with: "My Monster"
@@ -9,36 +9,34 @@ feature 'User creates a monster' do
 
     expect(page).to have_content "My Monster"
   end
-end
 
-feature 'User creates a monster with a trait', js: true do
-  scenario 'they see the monster on the page' do
-    visit new_monster_path
+  context 'with a trait', js: true do
+    scenario 'they see the trait on the page' do
+      visit new_monster_path
 
-    fill_in 'Name', with: "My Monster"
-    click_link "Add trait"
-    page.find('h3', text: "Traits").find(:xpath, '..')
-      .fill_in 'Name', with: "My Trait"
+      fill_in 'Name', with: "My Monster"
+      click_link "Add trait"
+      page.find('h3', text: "Traits").find(:xpath, '..')
+        .fill_in 'Name', with: "My Trait"
 
-    click_button "Create Monster"
+      click_button "Create Monster"
 
-    expect(page).to have_content "My Monster"
-    expect(page).to have_content "My Trait"
+      expect(page).to have_content "My Trait"
+    end
   end
-end
 
-feature 'User creates a monster with an action', js: true do
-  scenario 'they see the monster on the page' do
-    visit new_monster_path
+  context 'with an action', js: true do
+    scenario 'they see the action on the page' do
+      visit new_monster_path
 
-    fill_in 'Name', with: "My Monster"
-    click_link "Add action"
-    page.find('h3', text: "Actions").find(:xpath, '..')
-      .fill_in 'Name', with: "My Action"
+      fill_in 'Name', with: "My Monster"
+      click_link "Add action"
+      page.find('h3', text: "Actions").find(:xpath, '..')
+        .fill_in 'Name', with: "My Action"
 
-    click_button "Create Monster"
+      click_button "Create Monster"
 
-    expect(page).to have_content "My Monster"
-    expect(page).to have_content "My Action"
+      expect(page).to have_content "My Action"
+    end
   end
 end
