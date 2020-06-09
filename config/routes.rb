c9b96ae9ee7ae88dbd1712dede4f92b49907f6d1
rjclaasen <raphael.claasen@gmail.com>
2020-06-09 17:04:34 +0200

    Set root to monsters index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  root 'monsters#index'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
