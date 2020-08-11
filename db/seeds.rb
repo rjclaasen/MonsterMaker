be3b8fe903d2c027be67da1c982b0109821fbab6
Raphaël Claasen <raphael.claasen@gmail.com>
2020-08-11 18:24:56 +0200

    Add user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'factory_bot'
+
+user = User.create(email: "email@website.com", username: "username", password: "password")
+user.confirm
 
 50.times do
-  name = Faker::Games::ElderScrolls.creature
-  Monster.create!(name: name)
+  FactoryBot.create(:monster, user: user)
 end
